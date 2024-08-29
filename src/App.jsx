ead421c67e245eba42b5e0aead7561fc5c8ecdda
Daniel Pacheco <51678157+DaniellPacheco@users.noreply.github.com>
2024-08-29 13:06:04 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from "./App.module.css";
 
 function App() {
 
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Pedro Luiz"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, blanditiis nulla quas amet suscipit nisi odit doloribus nihil et deserunt dolor eum atque sequi odio quos provident ad! Aperiam, corporis."
+          />
+        </main>
+      </div>
     </div>
   )
 }
